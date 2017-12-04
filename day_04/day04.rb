0581dc21b7310b3bb64d1135d0991c425a463ace
Marc Baghdadi <marc_baghdadi@apple.com>
2017-12-04 12:28:33 +0000

    Day 4 Part 2 ruby

--- day_04/day04.rb
+++ day_04/day04.rb
@@ -9,4 +9,12 @@ end
 
 valid_p1_passphrases = passphrases.select { |pass| is_valid_p1_passphrase?(pass) }
 
-p valid_p1_passphrases.length
+def is_valid_p2_passphrase?(phrase)
+  post_uniq = phrase.map { |ps| ps.chars.sort(&:casecmp).join }.uniq
+  post_uniq.length == phrase.length
+end
+
+valid_p2_passphrases = passphrases.select { |pass| is_valid_p2_passphrase?(pass) }
+
+p "Part 1 valid passphrases: #{valid_p1_passphrases.length}"
+p "Part 2 valid passphrases: #{valid_p2_passphrases.length}"
